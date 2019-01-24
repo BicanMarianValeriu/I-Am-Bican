e7ea8c482fa42265aea07c168a488bb5dc8f985a
Bican Marian Valeriu <bican.valeriu@myzone.com>
2019-01-24 04:57:34 +0200

    assets

---
+++ build/precache-manifest.12b41157db52e29385cf0ef36a4f6586.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "46727951c294daa8b0a5cbc8f4162397",
+    "url": "/static/media/clock-o.46727951.svg"
+  },
+  {
+    "revision": "7d55770dd765792e5a8a",
+    "url": "/static/css/main.3f913f27.chunk.css"
+  },
+  {
+    "revision": "6b34053a68156dd8da77",
+    "url": "/static/js/portfolio.6b34053a.chunk.js"
+  },
+  {
+    "revision": "6881d5be405ee0439397",
+    "url": "/static/js/projects.6881d5be.chunk.js"
+  },
+  {
+    "revision": "3bbc9f451b336e227e31",
+    "url": "/static/js/swalcontact.3bbc9f45.chunk.js"
+  },
+  {
+    "revision": "d943e7286b69b34fc394",
+    "url": "/static/js/4.d943e728.chunk.js"
+  },
+  {
+    "revision": "801376623162dc41b96d",
+    "url": "/static/js/runtime~main.80137662.js"
+  },
+  {
+    "revision": "81ba79bb3fd7955d0e819cbb8d0280e7",
+    "url": "/static/media/placeholder-bold.81ba79bb.png"
+  },
+  {
+    "revision": "9d90f6b589b4b60b77a34b7c2d694c53",
+    "url": "/static/media/bican.9d90f6b5.jpg"
+  },
+  {
+    "revision": "19e754c923fa5e2c55add1c8902201cf",
+    "url": "/static/media/bican-old.19e754c9.jpg"
+  },
+  {
+    "revision": "7d55770dd765792e5a8a",
+    "url": "/static/js/main.7d55770d.chunk.js"
+  },
+  {
+    "revision": "d1b091fb92384f4a586c7844c3560669",
+    "url": "/static/media/banknote.d1b091fb.svg"
+  },
+  {
+    "revision": "731030645af57e8a32453fbe7ac8f041",
+    "url": "/static/media/website.73103064.svg"
+  },
+  {
+    "revision": "a0f406bf68dfefd7c894db5b8f3223fd",
+    "url": "/static/media/get-money.a0f406bf.svg"
+  },
+  {
+    "revision": "7a088887950c94ab6abc614def27f5ea",
+    "url": "/static/media/ShadowsIntoLight.7a088887.woff2"
+  },
+  {
+    "revision": "4e21f1ae69399656ab676b0e11d0c370",
+    "url": "/static/media/ShadowsIntoLight.4e21f1ae.woff"
+  },
+  {
+    "revision": "4141b2ff16a24a462106e206def679c1",
+    "url": "/static/media/ShadowsIntoLight.4141b2ff.ttf"
+  },
+  {
+    "revision": "33adae677fab86b44462b81620210061",
+    "url": "/static/media/ShadowsIntoLight.33adae67.eot"
+  },
+  {
+    "revision": "3c74097fbedcb8364bad02341117e51d",
+    "url": "/static/media/ShadowsIntoLight.3c74097f.svg"
+  },
+  {
+    "revision": "9fc32136231b1c75483e8c94affa5180",
+    "url": "/index.html"
+  }
+];
