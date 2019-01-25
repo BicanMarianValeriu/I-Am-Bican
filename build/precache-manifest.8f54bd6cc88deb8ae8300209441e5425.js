ea5a6b81fec6356594b38530041c78934a21b1b3
Bican Marian Valeriu <bican.valeriu@myzone.com>
2019-01-25 18:00:41 +0200

    assets

---
+++ build/precache-manifest.8f54bd6cc88deb8ae8300209441e5425.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d90f6b589b4b60b77a34b7c2d694c53",
+    "url": "/static/media/bican.9d90f6b5.jpg"
+  },
+  {
+    "revision": "930fac9382dbb122cf3c",
+    "url": "/static/css/main.b6e266f3.chunk.css"
+  },
+  {
+    "revision": "5fa2d13ca715f8d4a22e",
+    "url": "/static/js/portfolio.5fa2d13c.chunk.js"
+  },
+  {
+    "revision": "e4bebb21929c4d00b3a4",
+    "url": "/static/js/projects.e4bebb21.chunk.js"
+  },
+  {
+    "revision": "87556ae00e5efa76ce88",
+    "url": "/static/js/swalcontact.87556ae0.chunk.js"
+  },
+  {
+    "revision": "016fc697a820af2a7765",
+    "url": "/static/js/4.016fc697.chunk.js"
+  },
+  {
+    "revision": "8b839a6d06a034c58758",
+    "url": "/static/js/runtime~main.8b839a6d.js"
+  },
+  {
+    "revision": "81ba79bb3fd7955d0e819cbb8d0280e7",
+    "url": "/static/media/placeholder-bold.81ba79bb.png"
+  },
+  {
+    "revision": "930fac9382dbb122cf3c",
+    "url": "/static/js/main.930fac93.chunk.js"
+  },
+  {
+    "revision": "19e754c923fa5e2c55add1c8902201cf",
+    "url": "/static/media/bican-old.19e754c9.jpg"
+  },
+  {
+    "revision": "7a088887950c94ab6abc614def27f5ea",
+    "url": "/static/media/ShadowsIntoLight.7a088887.woff2"
+  },
+  {
+    "revision": "4e21f1ae69399656ab676b0e11d0c370",
+    "url": "/static/media/ShadowsIntoLight.4e21f1ae.woff"
+  },
+  {
+    "revision": "33adae677fab86b44462b81620210061",
+    "url": "/static/media/ShadowsIntoLight.33adae67.eot"
+  },
+  {
+    "revision": "4141b2ff16a24a462106e206def679c1",
+    "url": "/static/media/ShadowsIntoLight.4141b2ff.ttf"
+  },
+  {
+    "revision": "3c74097fbedcb8364bad02341117e51d",
+    "url": "/static/media/ShadowsIntoLight.3c74097f.svg"
+  },
+  {
+    "revision": "17e6d51116d54259f3da9bc39cec33d3",
+    "url": "/index.html"
+  }
+];
