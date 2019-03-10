acd3fcd87226c50c139d329a6cc7d735c5c6d303
Bican Marian Valeriu <bican.valeriu@myzone.com>
2019-03-10 23:04:22 +0200

    build

---
+++ build/precache-manifest.03eb45c0f23bcef8eda4b6e544abad2c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5df562da9da35ffe33b",
+    "url": "/static/js/swalcontact.013fd41a.chunk.js"
+  },
+  {
+    "revision": "54d09a95ebcf99ef8950",
+    "url": "/static/js/0.a2c471f7.chunk.js"
+  },
+  {
+    "revision": "47b69af38dec30a4fc7d",
+    "url": "/static/js/404.b28b8819.chunk.js"
+  },
+  {
+    "revision": "81ba79bb3fd7955d0e819cbb8d0280e7",
+    "url": "/static/media/placeholder-bold.81ba79bb.png"
+  },
+  {
+    "revision": "8baac3576984d5a18f85",
+    "url": "/static/js/main.3ebbf766.chunk.js"
+  },
+  {
+    "revision": "456497a834b9e93a3c95",
+    "url": "/static/js/page.f61b7893.chunk.js"
+  },
+  {
+    "revision": "3c74097fbedcb8364bad02341117e51d",
+    "url": "/static/media/ShadowsIntoLight.3c74097f.svg"
+  },
+  {
+    "revision": "14543b6d13fdd322c352",
+    "url": "/static/js/portfolio.f8d3d89e.chunk.js"
+  },
+  {
+    "revision": "4141b2ff16a24a462106e206def679c1",
+    "url": "/static/media/ShadowsIntoLight.4141b2ff.ttf"
+  },
+  {
+    "revision": "08b872245b5d2f80fbf0",
+    "url": "/static/js/projects.55a70199.chunk.js"
+  },
+  {
+    "revision": "cfe5771640e3b63d2689",
+    "url": "/static/js/runtime~main.d1886b94.js"
+  },
+  {
+    "revision": "832fbe739b44311ff2c0",
+    "url": "/static/js/swal-auth.2d0d78aa.chunk.js"
+  },
+  {
+    "revision": "7a088887950c94ab6abc614def27f5ea",
+    "url": "/static/media/ShadowsIntoLight.7a088887.woff2"
+  },
+  {
+    "revision": "33adae677fab86b44462b81620210061",
+    "url": "/static/media/ShadowsIntoLight.33adae67.eot"
+  },
+  {
+    "revision": "16fddc2c2130844ca90a",
+    "url": "/static/js/9.fc588ca8.chunk.js"
+  },
+  {
+    "revision": "9d90f6b589b4b60b77a34b7c2d694c53",
+    "url": "/static/media/bican.9d90f6b5.jpg"
+  },
+  {
+    "revision": "19e754c923fa5e2c55add1c8902201cf",
+    "url": "/static/media/bican-old.19e754c9.jpg"
+  },
+  {
+    "revision": "75c99c888e34bc33b9079a1b909d98c6",
+    "url": "/static/media/parallax-1.75c99c88.jpg"
+  },
+  {
+    "revision": "4e21f1ae69399656ab676b0e11d0c370",
+    "url": "/static/media/ShadowsIntoLight.4e21f1ae.woff"
+  },
+  {
+    "revision": "08b872245b5d2f80fbf0",
+    "url": "/static/css/projects.c7ceeff3.chunk.css"
+  },
+  {
+    "revision": "14543b6d13fdd322c352",
+    "url": "/static/css/portfolio.3813e821.chunk.css"
+  },
+  {
+    "revision": "8baac3576984d5a18f85",
+    "url": "/static/css/main.093022ad.chunk.css"
+  },
+  {
+    "revision": "16fddc2c2130844ca90a",
+    "url": "/static/css/9.6b2d2ad5.chunk.css"
+  },
+  {
+    "revision": "47b69af38dec30a4fc7d",
+    "url": "/static/css/404.b582dffa.chunk.css"
+  },
+  {
+    "revision": "382b807ea5bf66136243f967369f6e2a",
+    "url": "/index.html"
+  }
+];
